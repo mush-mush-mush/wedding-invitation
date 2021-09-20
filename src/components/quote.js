ef494f1043bde876f775db7983b4f969696e322e
mush-mush-mush <shroom.mish12@gmail.com>
2021-09-20 15:29:22 +0800

    v0.1.7 debug perfomance issues

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,36 +1,43 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-// import { motion, useAnimation } from "framer-motion";
-import React from "react";
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect } from "react";
 import image from "./../images/quote.jpg";
 import Bird from "./bird";
 import { useInView } from "react-intersection-observer";
 
 function Quote() {
   const { ref, inView } = useInView({ threshold: 0.2 });
-  // const animation = useAnimation();
+  const animation = useAnimation();
 
-  // useEffect(() => {
-  //   if (inView) {
-  //     animation.start({
-  //       opacity: 1,
-  //     });
-  //   } else {
-  //     animation.start({
-  //       opacity: 0,
-  //     });
-  //   }
-  // }, [inView]);
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        opacity: 1,
+      });
+    } else {
+      animation.start({
+        opacity: 0,
+      });
+    }
+  }, [inView]);
 
   return (
     <article ref={ref} className="quote container">
-      <section className=" d-flex justify-content-between align-items-center row">
-        <div className="quote--left col-sm-2 col-2">{inView && <Bird />}</div>
+      <motion.section
+        animate={animation}
+        className=" d-flex justify-content-between align-items-center row"
+      >
+        <div className="quote--left col-sm-2 col-2">
+          {
+            // inView && <Bird />
+          }
+        </div>
         <img src={image} alt="" className="quote--img col-sm-6 col-8"></img>
         <div className="quote--text">
           <h2>“He has made everything beautiful in its time.”</h2>
           <h5>Ecclesiastes 3:11</h5>
         </div>
-      </section>
+      </motion.section>
     </article>
   );
 }
--- src/components/wish.js
+++ src/components/wish.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState } from "react";
-// import { motion, useAnimation } from "framer-motion";
-// import { useInView } from "react-intersection-observer";
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import image from "./../images/wish.jpg";
 
 function Wish() {
@@ -51,23 +51,23 @@ function Wish() {
       });
   };
 
-  // const { ref, inView } = useInView({ threshold: 0.2 });
-  // const animation = useAnimation();
+  const { ref, inView } = useInView({ threshold: 0.2 });
+  const animation = useAnimation();
 
-  // useEffect(() => {
-  //   if (inView) {
-  //     animation.start({
-  //       opacity: 1,
-  //     });
-  //   } else {
-  //     animation.start({
-  //       opacity: 0,
-  //     });
-  //   }
-  // }, [inView]);
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        opacity: 1,
+      });
+    } else {
+      animation.start({
+        opacity: 0,
+      });
+    }
+  }, [inView]);
 
   return (
-    <article className="wish container">
+    <motion.article ref={ref} animate={animation} className="wish container">
       <section className="wish--container row px-0 d-flex justify-content-center">
         <div className="wish--image px-0 col-lg-6 col-md-4">
           <img src={image} alt="" className=""></img>
@@ -188,7 +188,7 @@ function Wish() {
           </form>
         </div>
       </section>
-    </article>
+    </motion.article>
   );
 }
 
