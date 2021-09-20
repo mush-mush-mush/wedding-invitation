c9cbb235902dd98f1cd99209bdf90f0039c7c561
mush-mush-mush <shroom.mish12@gmail.com>
2021-09-20 15:08:51 +0800

    v0.1.7 debug perfomance issues

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ delay: 0.5 }}
+      transition={{ delay: 1 }}
     >
       <Header />
       <Quote />
